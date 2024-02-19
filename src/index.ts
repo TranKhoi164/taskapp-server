5ad239b22482d389fd66f357037c190802074a0b
khoitran164 <khoitay11566@gmail.com>
2024-02-19 17:22:21 +0700

    vercel.json change

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,12 @@ dotenv.config()
 const app = new App(Number(process.env.PORT))
 
 app.listen()
+
+// {
+//   "src": "/(.*)",
+//   "dest": "/",
+//   "methods": ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//   "headers": {
+//     "Access-Control-Allow-Origin": "*"
+//   }
+// }
